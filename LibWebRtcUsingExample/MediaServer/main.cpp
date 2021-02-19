ce989b92d249a9c6123589b6e9fa1b3d367dfb53
HATTER-LONG <caolei6767@gmail.com>
2021-02-19 19:30:26 +0800

    编程小记

---
+++ LibWebRtcUsingExample/MediaServer/main.cpp
@@ -0,0 +1,8 @@
+#include "server.h"
+
+int main(int argc, char* argv[])
+{
+    auto* server = new MServer::Server {};
+    server->run();
+    return 0;
+}
---
+++ LibWebRtcUsingExample/MediaServer/server.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace MServer
+{
+class Server
+{
+public:
+    Server();
+    ~Server();
+
+public:
+    void run();
+};
+}   // namespace MServer
